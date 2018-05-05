449236c3938756d39fefd83339a897ebad6ff122
Zack Soliman <zack.soliman@gmail.com>
2018-05-05 16:59:05 -0400

    tree api

---
+++ app/api.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+
+
+app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'hackqc-2018'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/hackqc-2018'
+
+mongo = PyMongo(app)
+
+@app.route('/trees', methods=['GET'])
+def get_all_trees():
+  trees = mongo.db.trees
+  lat_long_sw = request.args.get('latlon_sw')
+  lat_long_ne = request.args.get('latlon_ne')
+  lat_sw, long_sw = [float(i) for i in lat_long_sw.split(',')]
+  lat_ne, long_ne = [float(i) for i in lat_long_ne.split(',')]
+
+  selected_trees = trees.find({'loc': {'$geoWithin': {
+      '$box': [
+          [long_ne, lat_ne],
+          [long_sw, lat_sw]
+      ]
+  }}})
+
+  output = []
+
+  for tree in selected_trees:
+      output.append(
+          {
+              "loc": tree["loc"],
+              "arrond_name": tree["arrond_nom"],
+              "tree_name_en": tree["essence_ang"],
+              "tree_name_fr": tree["essence_fr"],
+              "up_votes": tree["up_votes"],
+              "down_votes": tree["down_votes"]
+          })
+
+  return jsonify({'result': output})
+
+if __name__ == '__main__':
+    app.run(debug=True)
