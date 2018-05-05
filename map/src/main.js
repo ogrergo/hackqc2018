c89dda3ed9f465b68e1a95dc8040c69029f7b362
Louis van Beurden <louis.vanbeurden@gmail.com>
2018-05-05 13:27:30 -0400

    update

--- map/src/main.js
+++ map/src/main.js
@@ -1,4 +1,4 @@
-var map = L.map('mapid').setView([45.5, -73.8], 12);
+var map = L.map('mapid').setView([45.5, -73.8], 18);
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -7,22 +7,24 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   accessToken: 'pk.eyJ1Ijoib2dyZWJ1cmRlbiIsImEiOiJjamdyYmdyaXMwMzBsMzJueDlwdjBjZXlpIn0.Rar0bZ870hpisFVD7XwRQw'
 }).addTo(map);
 
-var data = require('./data.js');
+var trees = require('./trees.js')
+var geojsonMarkerOptions = {
+    radius: 3,
+    fillColor: "#22ff44",
+    color: "#000",
+    weight: 0.5,
+    opacity: 1,
+    fillOpacity: 0.5
+};
 
-L.geoJSON(data.parcs,  {
-    style: function (feature) {
-        return {color: '#33aaff'};
+console.log(trees)
+L.geoJSON(trees,{
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
     }
 }).bindPopup(function (layer) {
-    return layer.feature.properties.Nom_parc;
+    return layer.feature.properties.type;
 }).addTo(map);
 
-const colors = {'Mineure': '#882211', 'Modérée': '#aa2211', 'Majeure': '#ee3322'}
-
-L.geoJSON(data.vague_de_chaleur,  {
-    style: function (feature) {
-        return {color: colors[feature.properties.VULN_CAT]};
-    }
-}).bindPopup(function (layer) {
-    return layer.feature.properties.NOM_ARR;
-}).addTo(map);
+map.panTo(new L.LatLng(45.517711,-73.5966052));
+// L.control.mousePosition().addTo(map);
