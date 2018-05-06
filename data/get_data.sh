569097680ef1a31726d33a4e4917cc19e9ce5cd1
Zack Soliman <zack.soliman@gmail.com>
2018-05-06 00:47:10 -0400

    Adds Script to download tree datasets

---
+++ data/get_data.sh
@@ -0,0 +1,3 @@
+wget http://donnees.ville.montreal.qc.ca/dataset/3e3efad6-9f2f-4cc0-8f1b-92de1ccdb282/resource/c6c5afe8-10be-4539-8eae-93918ea9866e/download/arbres-publics.csv
+wget https://www.donneesquebec.ca/recherche/fr/dataset/34103a43-3712-4a29-92e1-039e9188e915/resource/13a51853-a5b5-4add-8791-02ccba5c1be7/download/vdq-arbrerepertorie.csv
+wget https://www.donneesquebec.ca/recherche/fr/dataset/d65f10dd-9948-4b52-bc4c-b40261e2c593/resource/0ab4da5a-b470-4774-9f2a-4d9bb19763a5/download/arbres.csv
